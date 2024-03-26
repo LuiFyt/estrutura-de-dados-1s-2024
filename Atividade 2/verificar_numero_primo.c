040b0e6780739bbf84ca0d5186200e715acf0865
Luiz Fernando <luizfer0411@gmail.com>
2024-03-26 20:56:56 -0300

    Codigo Finalizado

--- Atividade 2/verificar_numero_primo.c
+++ Atividade 2/verificar_numero_primo.c
@@ -3,7 +3,19 @@
 
 int main(int argc, char* argv[]) {
 
-    int n = atoi (argv[1]);
+    int n = atoi (argv[1]), i, r = 0;
+
+    for(i = 1; i <= n; i++) {
+        if(n % i == 0) {
+            r++;
+        }
+    }
+
+    if(r == 2) {
+        printf("True");
+    }else {
+        printf("False");
+    }
 
     exit(0);
 }
